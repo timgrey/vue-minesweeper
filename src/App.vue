38f6557ab3374cf34b274a69ea32843612b6d684
Tim grey <timothy.a.grey@gmail.com>
2020-11-06 13:47:40 -0600

    vue init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <vue-minesweeper />
+  </div>
+</template>
+
+<script>
+import VueMinesweeper from './components/VueMinesweeper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VueMinesweeper
+  }
+}
+</script>
